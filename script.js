e34126a8dfde13d9beb3071487be43193d2ffb2f
Akash Kumar <151871443+Akki789@users.noreply.github.com>
2025-10-04 16:11:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+  // stop the form from submitting and reloading the page
+  event.preventDefault();
 
+  // get values from the form fields
+  const firstName = document.forms["form1"]["fname"].value;
+  const lastName = document.forms["form1"]["lname"].value;
+
+  // show the full name
+  alert(firstName + " " + lastName);
 }
