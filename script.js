21f26312bdb3175811c0b4d212ce96abecb9a6ba
Akash Kumar <151871443+Akki789@users.noreply.github.com>
2025-10-04 16:46:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function getFormvalue() {
-	let form = document.getElementById("#form1");
   const firstName = document.forms["form1"]["fname"].value;
   const lastName = document.forms["form1"]["lname"].value;
 
-	form.addEventListener("submit", () =>{
-		alert(firstName + " " + lastName);
-	});
-  
+  alert(firstName + " " + lastName);
+
+  return false; // stops form submission without needing event.preventDefault()
 }
